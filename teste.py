6b9d6ed25168a15a33dec43ba73e2ee936255890
AND2597 <andrendelgado@gmail.com>
2022-03-08 10:29:59 -0300

    retirar o M do lado do nome do arquivo

--- teste.py
+++ teste.py
@@ -6,7 +6,8 @@ print('Teste de submição no repositório')
     ### git commit -m ' Comentário a ser add junto a modificação feita nos doc's ou doc'
     ### git pull --------- extrai do repositório todos os arquivos nele presentes naquele momento
     ### git push --------- envia ao repositório todos so arquivos locais da sua máquina
+    #######  deve se se salvar e add o arquivo novamente no local (sua maquina)
     #######  não deixar o 'M' que aparece ao lado do nome do arquivo, pois ele não atualiza o repositório, deve-se da um
-    #######  novo commit para atualizar 
-    
+    #######  novo commit para atualizar apos salvar e add 
+
 
