d5c3f5877e9f0423737420adcc989492de537397
AND2597 <andrendelgado@gmail.com>
2022-03-08 10:18:30 -0300

    Nova submissão

--- teste.py
+++ teste.py
@@ -1,1 +1,9 @@
 print('Teste de submição no repositório')
+### Comandos no Git Bash:
+    ### git clone 'link do repositório' -------- Faz a clonagem do repositório para o seu computador
+### Comandos no terminla do Power shell:
+    ### git add . ou git add 'nome do arquivo' ______ Adiciona o arquivo a pasta local
+    ### git commit -m ' Comentário a ser add junto a modificação feita nos doc's ou doc'
+    ### git pull --------- extrai do repositório todos os arquivos nele presentes naquele momento
+    ### git push --------- envia ao repositório todos so arquivos locais da sua máquina
+
